d2e4ec884092d645f600687774dc10bd62d12954
anubhavj33 <44263678+anubhavj33@users.noreply.github.com>
2023-02-27 14:22:42 +0530

    Add files via upload

---
+++ JDBCexpamle/src/main/java/com/example/JDBCexpamle/JdbCexpamleApplication.java
@@ -0,0 +1,13 @@
+package com.example.JDBCexpamle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbCexpamleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbCexpamleApplication.class, args);
+	}
+
+}
---
+++ JDBCexpamle/src/test/java/com/example/JDBCexpamle/JdbCexpamleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JDBCexpamle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbCexpamleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
